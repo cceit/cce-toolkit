48c2067af62007104d766d8d196dfeca1a923e79
Alaina Troiano <atroiano@ou.edu>
2015-08-18 12:53:54 -0500

    more up-to-date setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-    name = 'cce_toolkit',
-    packages = ['cce_toolkit'], # this must be the same as the name above
-    version = '0.1',
-    description = 'Utils for CCE-IT developers (joint authorship)',
-    author = 'Alaina Rowe',
-    author_email = 'atroiano@ou.edu',
-    # url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-    # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-    keywords = ['CCE-IT', 'tools', 'utils'],
-    classifiers = [],
+    name = "cce_toolkit",
+    version = "0.1",
+    packages = find_packages(),
 )
 
