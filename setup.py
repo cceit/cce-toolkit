8d1f9d48ffc572c451cc3ddfd3f847b7b211519e
Alaina Troiano <atroiano@ou.edu>
2015-08-18 14:44:55 -0500

    taking out some options I probably didn't need

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 setup(
+    # Metadata for PyPI; nice to have but not required
     name="cce_toolkit",
     version="0.1",
-    url='ssh://code.ce.ou.edu/var/git/cce_toolkit',
-    packages=['toolkit'],
-    package_dir={'toolkit': 'toolkit'},  # this shouldn't be necessary
-    include_package_data=True,
-    package_data={'toolkit': ['*.py','mixins/*.py','templatetags/*.py','test_tools/*.py','test_tools/steps/*.py']},
     description='A collection of tools for use in CCE-IT projects',
     author='CCE Devs',
     license='All Rights Reserved',
-    zip_safe=False,
+    # I don't think this url is required either, but it's nice to have
+    url='ssh://code.ce.ou.edu/var/git/cce_toolkit',
+    packages=['toolkit'],
+    zip_safe=False,  # important, forces it to install as directories and not .zip
 )
 
