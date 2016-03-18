99ceafddd5db35ecdc4ce97743f6712c11c717c8
Michael <mhillebrand@ou.edu>
2016-03-18 15:20:06 -0500

    added datetimepicker to inialize_plugins

--- toolkit/static/toolkit/plugins.js
+++ toolkit/static/toolkit/plugins.js
@@ -15,6 +15,7 @@ function initialize_plugins(advanced_search_form_bound) {
     $('.timefield').datetimepicker({
         format: 'LT'
     });
+    $('.datetimefield').datetimepicker();
 
     var $advance_search_toggle = $('#advance_search_toggle');
     var $advanced_search_form = $('#advanced_search_form');
