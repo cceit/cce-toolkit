a37227376534a4572f2cf9301a112b76a2cee491
charrison <charrison@ou.edu>
2016-04-05 09:55:26 -0500

    Adding naive datetime fix

--- toolkit/models.py
+++ toolkit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cuser.fields import CurrentUserField
+from django.utils import timezone
 
 from mixins.models import ModelPermissionsMixin
 
@@ -17,9 +18,9 @@ class CCEAuditModel(CCEModel):
     Abstract model with fields for the user and timestamp of a row's creation and last update.
     """
     last_updated_by = CurrentUserField(related_name='%(app_label)s_%(class)s_last_updated')
-    last_updated_at = models.DateTimeField(auto_now=True)
+    last_updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
     created_by = CurrentUserField(add_only=True, related_name='%(app_label)s_%(class)s_last_created')
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
 
     class Meta:
         abstract = True
