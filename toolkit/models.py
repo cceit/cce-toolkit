5b05470a509ff18ca66d2dd91d15bd6e706e8afa
charrison <charrison@ou.edu>
2016-04-05 10:23:57 -0500

    Revert naive datetime fix

--- toolkit/models.py
+++ toolkit/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from cuser.fields import CurrentUserField
-from django.utils import timezone
 
 from mixins.models import ModelPermissionsMixin
 
@@ -18,9 +17,9 @@ class CCEAuditModel(CCEModel):
     Abstract model with fields for the user and timestamp of a row's creation and last update.
     """
     last_updated_by = CurrentUserField(related_name='%(app_label)s_%(class)s_last_updated')
-    last_updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
+    last_updated_at = models.DateTimeField(auto_now=True)
     created_by = CurrentUserField(add_only=True, related_name='%(app_label)s_%(class)s_last_created')
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
