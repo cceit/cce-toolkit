6f150871902f93f5cabf6b32987776a4bd8a7067
charrison <charrison@ou.edu>
2016-06-09 13:29:18 -0500

    Adding AppConfig for ActivityLog

--- toolkit/activity_log/apps.py
+++ toolkit/activity_log/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ActivityLogConfig(AppConfig):
     name = "activity_log"
     verbose_name = "Activity Log"
-
-    def ready(self):
-        # noinspection PyUnresolvedReferences
-        from . import signals
