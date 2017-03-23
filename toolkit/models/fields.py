5bb6c6ecaf1f5a78dbf7a75f3ba99de3fb8242c2
charrison <charrison@ou.edu>
2017-03-23 08:42:40 -0500

    Adds CurrencyField

--- toolkit/models/fields.py
+++ toolkit/models/fields.py
@@ -4,7 +4,7 @@ from django.db.models import fields
 class CurrencyField(fields.DecimalField):
     """Represents an amount of USD"""
 
-    def __init__(self, max_digits=None, decimal_places=None, **kwargs):
-        self.max_digits = max_digits or 12
-        self.decimal_places = decimal_places or 2
+    def __init__(self, **kwargs):
+        self.max_digits = 12
+        self.decimal_places = 2
         super(CurrencyField, self).__init__(**kwargs)
