8fbd31693ed75dd70d31447051745c8ea4f90090
charrison <charrison@ou.edu>
2017-03-23 09:08:54 -0500

    Adds CurrencyField

--- toolkit/models/fields.py
+++ toolkit/models/fields.py
@@ -1,10 +1,13 @@
-from django.db.models import fields
+from django.db.models.fields import DecimalField
+from django.utils.translation import ugettext_lazy as _
 
 
-class CurrencyField(fields.DecimalField):
+class CurrencyField(DecimalField):
     """Represents an amount of USD"""
+    description = _("USD Currency value")
 
-    def __init__(self, **kwargs):
-        self.max_digits = 12
-        self.decimal_places = 2
-        super(CurrencyField, self).__init__(**kwargs)
+    def __init__(self, verbose_name=None, name=None, max_digits=None, decimal_places=None, **kwargs):
+        super(CurrencyField, self).__init__(
+            verbose_name, name, max_digits=(max_digits or 12), decimal_places=(decimal_places or 2),
+            **kwargs
+        )
