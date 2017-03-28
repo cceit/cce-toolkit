da7c3bf1f02bbc22260d4e67a33d3468f23b3afc
rlaw <rlaw@ou.edu>
2017-03-28 09:59:54 -0500

    OCMS-123: added spectrum colorpicker

---
+++ toolkit/forms/widgets/spectrum_colorpicker.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django import forms
+from django.utils.safestring import mark_safe
+
+"""Based on Spectrum Colorpicker
+http://bgrins.github.io/spectrum/
+"""
+
+
+class SpectrumColorPickerWidget(forms.TextInput):
+    class Media:
+        js = ['//cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js']
+
+    def render(self, name, value, attrs=None):
+        rendered = super(SpectrumColorPickerWidget, self).render(name, value, attrs)
+        return rendered + mark_safe(u'''<script type="text/javascript">
+                        $(function() {
+                            $('#id_%s').spectrum({
+                                showInput: true,
+                                className: "full-spectrum",
+                                showInitial: true,
+                                preferredFormat: "hex",
+                                localStorageKey: "spectrum.demo",
+                                allowEmpty:true,
+                            });
+                        });
+                    </script>''' % name)
+
+
+class SpectrumColorPickerField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 10
+        super(SpectrumColorPickerField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        kwargs['widget'] = SpectrumColorPickerWidget
+        return super(SpectrumColorPickerField, self).formfield(**kwargs)
